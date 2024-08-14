0d4b251abafa9a0d10ea39768bd62e5bdd2cf917
nabinkhair42 <nabinkhair12@gmail.com>
2024-08-15 05:10:03 +0545

    Share Feature Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
-
+ 
 export const metadata: Metadata = {
   title: "Express Newz: Fun, Relaxed and Unbiased",
   description:
