449d28f8f7c59b072e461f2094808f04542f113a
nabinkhair42 <nabinkhair12@gmail.com>
2024-08-22 21:36:25 +0545

    chore: Remove unused import and fix typo in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import GoldPrice from "@/components/widgets/GoldPrice";
-import RecentPost from "@/components/widgets/TrendingPost";
 import React from "react";
 import NewsPage from "./(pages)/news/page";
 import { Carousel } from "@/components/onUse/stories";
@@ -15,7 +14,7 @@ const page = () => {
         <NewsPage />
         <div className="flex flex-col gap-4 mt-4 md:flex-row items-center md:items-start xl:flex-col">
           <GoldPrice />
-          <TrendingPost />
+          <TrendingPost />g
           <CategorizedPost />
         </div>
       </div>
